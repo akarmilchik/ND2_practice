b477989c071fa9cfabfe69c3fab5eda72c0780f5
Alexey <alexey.karm@mail.ru>
2020-08-30 22:23:19 +0300

    1.10 Add test class using NUnit

---
+++ CarServiceLibrary_Karm.Test/CarTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace CarServiceLibrary_Karm.Test
+{
+    [TestFixture]
+    public class CarTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
